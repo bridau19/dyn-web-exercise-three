69d835ac8e3e757f496ef964dcbf73a2d19e8438
bridau19 <bridau19@gmail.com>
2021-10-21 08:34:16 -0400

    add all files

---
+++ routes/about.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send(`
+        <h1>About this page</h1>
+        <p>This is a sentence about this page.</p>
+        <p><a href="/about/me">Learn about me</a></p>
+        `);
+});
+
+router.get('/me', (req, res) => {
+    res.send(`
+    <h1>About me</h1>
+    `);
+});
+
+module.exports = router;
